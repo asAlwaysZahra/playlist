ba99a581bfc2bfd0d2a8f800cda9fd7fd3e68b8d
Zahra <zahraamasoumi@gmail.com>
2022-10-24 10:11:23 +0330

    init project

---
+++ PlayList/src/test/java/com/example/PlayList/PlayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
